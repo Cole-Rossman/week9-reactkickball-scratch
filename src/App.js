608524d7a9c13d2ef154cd438fb3cdcee401c886
Cole Rossman <colerossman99@gmail.com>
2022-03-23 16:14:17 -0700

    App.js uses browserrouter effectively

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import NavHeader from './components/NavHeader/NavHeader';
+import Home from './views/Home/Home';
+import Players from './views/Players/Players';
+import PlayerDetail from './views/Players/PlayerDetail';
+import Teams from './views/Teams/Teams';
+import TeamDetail from './views/Teams/TeamDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavHeader />
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/players">
+            <Players />
+          </Route>
+          <Route path="/players/:id">
+            <PlayerDetail />
+          </Route>
+          <Route exact path="/teams">
+            <Teams />
+          </Route>
+          <Route path="/teams/:id">
+            <TeamDetail />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavHeader/NavHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NavHeader() {
+  return (
+    <div>NavHeader</div>
+  );
+}
