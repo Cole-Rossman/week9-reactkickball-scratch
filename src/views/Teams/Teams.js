3610a8e90543f5fbfd91860641d1d0e39e2e5c26
Cole Rossman <colerossman99@gmail.com>
2022-03-23 17:08:55 -0700

    teams.js fetches all teams and Teams.js ureturns list of teams with link to click

--- src/views/Teams/Teams.js
+++ src/views/Teams/Teams.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchTeams } from '../../services/teams';
+import { Link } from 'react-router-dom';
 
 export default function Teams() {
+  const [teams, setTeams] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const teamData = await fetchTeams();
+        console.log(teamData);
+        setTeams(teamData);
+      } catch (e) {
+        setError(e.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>Teams</div>
+    <div>
+      <h1>Teams</h1>
+      <ul className='team-list'>
+        {error && <p>{error}</p>}
+        {teams.map((team) => (
+          <Link key={team.id} to={`./teams/${team.id}`} >
+            <li key={team.id}>{team.name}</li>
+          </Link>
+        ))}
+      </ul>
+    </div>
   );
 }
